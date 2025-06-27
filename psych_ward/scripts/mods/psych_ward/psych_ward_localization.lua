02a2240ef33ac932b320e0b5576022242725e474
SyuanTsai <carsun00@Gmail.com>
2025-06-27 17:47:22 +0800

    Add Psych ward

--- psych_ward/scripts/mods/psych_ward/psych_ward_localization.lua
+++ psych_ward/scripts/mods/psych_ward/psych_ward_localization.lua
@@ -6,6 +6,7 @@ mod:add_global_localize_strings({
     ["zh-cn"] = "大厅功能",
     ru = "Торговцы",
     ja = "ベンダー",
+    ["zh-tw"] = "大廳功能",
   }
 })
 
@@ -14,91 +15,107 @@ return {
     en = "Psych Ward",
     ["zh-cn"] = "快捷访问",
     ru = "Психушка",
+    ["zh-tw"] = "靈能室",
   },
   psych_ward_description = {
     en = "Provides access to many Hub features from the Character Select menu.",
     ["zh-cn"] = "直接在角色选择页面使用各种大厅功能。",
     ru = "Psych Ward - Позволяет войти прямо на стрельбище в Псайканиум, минуя Хаб.",
     ja = "キャラクター選択画面から様々なハブ機能へとアクセスできるようになります。",
+    ["zh-tw"] = "啟用直接進入靈能室功能，無需進入大廳。",
   },
   horde_button = {
     en = "Mortis Trials",
     ["zh-cn"] = "死灵试炼",
     ru = "Стрельбище",
     ja = "モーティスの試練",
+    ["zh-tw"] = "死神試煉",
   },
   mission_button = {
     en = "Mission Board",
     ja = "ミッションボード",
     ["zh-cn"] = "任务面板",
     ru = "Меню выбора миссий",
+    ["zh-tw"] = "任務面板",
   },
-  vendor_button= {
+  vendor_button = {
     en = "Armoury",
     ["zh-cn"] = "军械交易所",
     ru = "Оружейная",
     ja = "武器交換",
+    ["zh-tw"] = "軍械交易所",
   },
   contracts_button = {
     en = "Contracts",
     ["zh-cn"] = "每周协议",
     ru = "Контракты",
     ja = "週間契約",
+    ["zh-tw"] = "領主商店",
   },
   crafting_button = {
     en = "Crafting",
     ["zh-cn"] = "锻造",
     ru = "Кузница",
     ja = "クラフト",
+    ["zh-tw"] = "鍛造",
   },
   cosmetics_button = {
     en = "Cosmetics",
     ru = "Интендант",
     ["zh-cn"] = "装饰品",
     ja = "装飾品",
+    ["zh-tw"] = "雜貨店",
   },
   penance_button = {
     en = "Penances",
     ru = "Искупления",
     ["zh-cn"] = "苦修",
     ja = "苦行",
+    ["zh-tw"] = "苦修",
   },
   inventory_button = {
     en = "Inventory",
     ["zh-cn"] = "库存",
     ru = "Инвентарь",
     ja = "インベントリ",
+    ["zh-tw"] = "裝備",
   },
   havoc_button = {
     en = "Havoc Mission",
     ["zh-cn"] = "浩劫任务",
     ja = "ハヴォック任務",
+    ["zh-tw"] = "浩劫任務",
   },
   meatgrinder_button = {
     en = "Meat Grinder",
     ["zh-cn"] = "绞肉机",
     ja = "肉挽き機"
+    ["zh-tw"] = "絞肉機",
   },
   exit_text = {
     en = "Press %s to quit",
     ja = "%sで終了",
     ["zh-cn"] = "按下%s以退出",
     ru = "Нажмите %s, чтобы выйти",
+    ["zh-tw"] = "按下%s以離開",
   },
   cutscenes_hub_only = {
     en = "Only Viewable from the Hub",
     ["zh-cn"] = "仅可在大厅内查看",
     ru = "Можно посмотреть только в Хабе",
     ja = "ハブでのみ閲覧可能",
+    ["zh-tw"] = "僅可在大廳內查看",
   },
   enter_hub = {
     en = "Enter Hub",
     ["zh-cn"] = "进入大厅",
     ja = "ハブに行く",
+    ["zh-tw"] = "進入大廳",
   },
   allow_chat_main_menu = {
     en = "Allow Chat in Character Select screen",
     ["zh-cn"] = "允许在角色选择界面使用聊天框",
     ja = "キャラクター選択画面でのチャットを許可",
+    ["zh-tw"] = "允許在角色選擇界面使用聊天框",
   },
 }
