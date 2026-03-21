822d7e33d7afecb024c4c5be8a80dd4295bcddae
xsSplater - СанСаныч <xssplater@gmail.com>
2026-03-22 01:22:52 +0300

    Update psych_ward_data.lua

--- psych_ward/scripts/mods/psych_ward/psych_ward_data.lua
+++ psych_ward/scripts/mods/psych_ward/psych_ward_data.lua
@@ -21,14 +21,14 @@ return {
             type = "numeric",
             default_value = -5,
             range = {-950, 950},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_inventory_button_pos_v",
             type = "numeric",
-            default_value = -390,
+            default_value = -370,
             range = {-1050, 0},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_inventory_button_size_h",
@@ -56,14 +56,14 @@ return {
             type = "numeric",
             default_value = -5,
             range = {-950, 950},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_crafting_button_pos_v",
             type = "numeric",
-            default_value = -345,
+            default_value = -320,
             range = {-1050, 0},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_crafting_button_size_h",
@@ -91,14 +91,14 @@ return {
             type = "numeric",
             default_value = -5,
             range = {-950, 950},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_vendor_button_pos_v",
             type = "numeric",
-            default_value = -300,
+            default_value = -270,
             range = {-1050, 0},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_vendor_button_size_h",
@@ -116,6 +116,7 @@ return {
           },
         },
       },
+
       -- MISSION BUTTON
       {
         setting_id = "pw_mission_button_pos_size",
@@ -124,16 +125,16 @@ return {
           {
             setting_id = "pw_mission_button_pos_h",
             type = "numeric",
-            default_value = 0,
+            default_value = -200,
             range = {-950, 950},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_mission_button_pos_v",
             type = "numeric",
             default_value = 40,
             range = {-1050, 150},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_mission_button_size_h",
@@ -145,12 +146,118 @@ return {
           {
             setting_id = "pw_mission_button_size_v",
             type = "numeric",
-            default_value = 60,
+            default_value = 50,
             range = {30, 70},
             step_size_value = 1,
           },
         },
       },
+      -- MORTIS BUTTON
+      {
+        setting_id = "pw_mortis_button_pos_size",
+        type = "group",
+        sub_widgets = {
+          {
+            setting_id = "pw_mortis_button_pos_h",
+            type = "numeric",
+            default_value = -465,
+            range = {-950, 950},
+            step_size_value = 5,
+          },
+          {
+            setting_id = "pw_mortis_button_pos_v",
+            type = "numeric",
+            default_value = 40,
+            range = {-1050, 150},
+            step_size_value = 5,
+          },
+          {
+            setting_id = "pw_mortis_button_size_h",
+            type = "numeric",
+            default_value = 250,
+            range = {150, 250},
+            step_size_value = 1,
+          },
+          {
+            setting_id = "pw_mortis_button_size_v",
+            type = "numeric",
+            default_value = 50,
+            range = {30, 70},
+            step_size_value = 1,
+          },
+        },
+      },
+      -- EXPEDITIONS BUTTON
+      {
+        setting_id = "pw_expeditions_button_pos_size",
+        type = "group",
+        sub_widgets = {
+          {
+            setting_id = "pw_expeditions_button_pos_h",
+            type = "numeric",
+            default_value = -730,
+            range = {-950, 950},
+            step_size_value = 5,
+          },
+          {
+            setting_id = "pw_expeditions_button_pos_v",
+            type = "numeric",
+            default_value = 40,
+            range = {-1050, 150},
+            step_size_value = 5,
+          },
+          {
+            setting_id = "pw_expeditions_button_size_h",
+            type = "numeric",
+            default_value = 250,
+            range = {150, 250},
+            step_size_value = 1,
+          },
+          {
+            setting_id = "pw_expeditions_button_size_v",
+            type = "numeric",
+            default_value = 50,
+            range = {30, 70},
+            step_size_value = 1,
+          },
+        },
+      },
+      -- MEATGRINDER BUTTON
+      {
+        setting_id = "pw_meatgrinder_button_pos_size",
+        type = "group",
+        sub_widgets = {
+          {
+            setting_id = "pw_meatgrinder_button_pos_h",
+            type = "numeric",
+            default_value = 0,
+            range = {-950, 950},
+            step_size_value = 5,
+          },
+          {
+            setting_id = "pw_meatgrinder_button_pos_v",
+            type = "numeric",
+            default_value = -15,
+            range = {-1050, 150},
+            step_size_value = 5,
+          },
+          {
+            setting_id = "pw_meatgrinder_button_size_h",
+            type = "numeric",
+            default_value = 220,
+            range = {150, 250},
+            step_size_value = 1,
+          },
+          {
+            setting_id = "pw_meatgrinder_button_size_v",
+            type = "numeric",
+            default_value = 50,
+            range = {30, 70},
+            step_size_value = 1,
+          },
+        },
+      },
+
       -- CONTRACTS BUTTON
       {
         setting_id = "pw_contracts_button_pos_size",
@@ -159,16 +266,16 @@ return {
           {
             setting_id = "pw_contracts_button_pos_h",
             type = "numeric",
-            default_value = 230,
+            default_value = -670,
             range = {-950, 950},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_contracts_button_pos_v",
             type = "numeric",
-            default_value = -75,
+            default_value = -1020,
             range = {-1050, 0},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_contracts_button_size_h",
@@ -180,13 +287,13 @@ return {
           {
             setting_id = "pw_contracts_button_size_v",
             type = "numeric",
-            default_value = 40,
+            default_value = 50,
             range = {30, 70},
             step_size_value = 1,
           },
         },
       },
-      -- PENANCE BUTTON
+      -- PENANCES BUTTON
       {
         setting_id = "pw_penance_button_pos_size",
         type = "group",
@@ -194,16 +301,16 @@ return {
           {
             setting_id = "pw_penance_button_pos_h",
             type = "numeric",
-            default_value = 0,
+            default_value = -435,
             range = {-950, 950},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_penance_button_pos_v",
             type = "numeric",
-            default_value = -75,
+            default_value = -1020,
             range = {-1050, 0},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_penance_button_size_h",
@@ -215,7 +322,7 @@ return {
           {
             setting_id = "pw_penance_button_size_v",
             type = "numeric",
-            default_value = 40,
+            default_value = 50,
             range = {30, 70},
             step_size_value = 1,
           },
@@ -229,16 +336,16 @@ return {
           {
             setting_id = "pw_cosmetics_button_pos_h",
             type = "numeric",
-            default_value = -230,
+            default_value = -200,
             range = {-950, 950},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_cosmetics_button_pos_v",
             type = "numeric",
-            default_value = -75,
+            default_value = -1020,
             range = {-1050, 0},
-            step_size_value = 1,
+            step_size_value = 5,
           },
           {
             setting_id = "pw_cosmetics_button_size_h",
@@ -250,7 +357,7 @@ return {
           {
             setting_id = "pw_cosmetics_button_size_v",
             type = "numeric",
-            default_value = 40,
+            default_value = 50,
             range = {30, 70},
             step_size_value = 1,
           },
